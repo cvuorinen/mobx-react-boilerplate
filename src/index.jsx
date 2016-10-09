cc3eddde9d3d4495ab0c97dac3a3f43aa4e01cc5
Carl Vuorinen <carl.vuorinen@gmail.com>
2016-10-09 15:10:19 +0300

    Implement todo list from getting started guide

--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import AppState from './AppState';
-import App from './App';
+import TodoList from './TodoList';
+import ObservableTodoStore from './ObservableTodoStore';
 
-const appState = new AppState();
+const observableTodoStore = new ObservableTodoStore();
 
 render(
-  <AppContainer>
-    <App appState={appState} />
-  </AppContainer>,
+  <TodoList store={ observableTodoStore } />,
   document.getElementById('root')
 );
-
-if (module.hot) {
-  module.hot.accept('./App', () => {
-    const NextApp = require('./App').default;
-
-    render(
-      <AppContainer>
-        <NextApp appState={appState} />
-      </AppContainer>,
-      document.getElementById('root')
-    );
-  });
-}
